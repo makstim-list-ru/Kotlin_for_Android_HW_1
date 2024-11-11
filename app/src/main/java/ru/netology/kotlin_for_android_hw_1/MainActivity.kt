03e8f982f3be9f10b10aa2922a8bdc4c6b1d9651
Maksim Timofeyev <makstim@list.ru>
2024-11-11 23:25:37 +0300

    Commit initial HW4

--- app/src/main/java/ru/netology/kotlin_for_android_hw_1/MainActivity.kt
+++ app/src/main/java/ru/netology/kotlin_for_android_hw_1/MainActivity.kt
@@ -5,10 +5,14 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlin.math.ln
+import kotlin.math.pow
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        println("cdaedcaefdcfefdcfcefc")
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
@@ -16,5 +20,14 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+
     }
+
+    fun getFormatedNumber(count: Long): String {
+        if (count < 1000) return "" + count
+        val exp = (ln(count.toDouble()) / ln(1000.0)).toInt()
+        return String.format("%.1f %c", count / 1000.0.pow(exp.toDouble()), "kMGTPE"[exp - 1])
+    }
+
 }
