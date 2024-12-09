4beb13d360ea152fc419249b319c28eaafcc3479
Maksim Timofeyev <makstim@list.ru>
2024-12-10 01:54:37 +0300

    Commit HW9 v1 initial

--- app/src/main/java/ru/netology/kotlin_for_android_hw_1/MainActivity.kt
+++ app/src/main/java/ru/netology/kotlin_for_android_hw_1/MainActivity.kt
@@ -1,5 +1,7 @@
 package ru.netology.kotlin_for_android_hw_1
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
@@ -17,11 +19,32 @@ class MainActivity : AppCompatActivity() {
 
         val viewModel by viewModels<PostViewModel>()
 
+        val getContent2 = registerForActivityResult(ContractMainActivity2) { result ->
+            result ?: return@registerForActivityResult
+            viewModel.saveVM(result)
+        }
+
         val adapter = PostsAdapter { post, key ->
             if (key == "like") viewModel.likeVM(post.id)
-            if (key == "share") viewModel.shareVM(post.id)
+            if (key == "share") {
+                val intent = Intent().apply {
+                    putExtra(Intent.EXTRA_TEXT, post.content)
+                    type = "text/plain"
+                    action = Intent.ACTION_SEND
+                }
+                val shareIntent = Intent.createChooser(intent, "Sharing the post")
+                startActivity(shareIntent)
+                viewModel.shareVM(post.id)
+            }
+            if (key == "video") {
+                val intent = Intent(Intent.ACTION_VIEW, Uri.parse(post.video))
+                startActivity(intent)
+            }
             if (key == "remove") viewModel.removeVM(post.id)
-            if (key == "edit") viewModel.editVM(post)
+            if (key == "edit") {
+                getContent2.launch(post.content)
+                viewModel.editVM(post)
+            }
             if (key == "cancel") viewModel.cancelVM()
         }
         binding.container.adapter = adapter
@@ -34,33 +57,37 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
-        viewModel.editedPostLD.observe(this) {
-            binding.content.setText(it.content)
-            binding.content.requestFocus()
-            if (it.id != 0L) {
-                binding.preview1.setText(it.author)
-                binding.preview2.setText(it.published)
-                binding.group1.setVisibility(View.VISIBLE)
-            }
-            else binding.group1.setVisibility(View.GONE)
+        binding.plusButton.setOnClickListener {
+            getContent2.launch(null)
         }
 
-        binding.saveButton.setOnClickListener {
-            val text = binding.content.text.toString()
-            if (text.isBlank()) {
-                Toast.makeText(this, "error empty content", Toast.LENGTH_LONG).show()
-                return@setOnClickListener
-            }
-            viewModel.saveVM(text)
-            binding.content.setText("")
-            binding.content.clearFocus()
-        }
 
-        binding.cancelEdit.setOnClickListener {
-            binding.content.setText("")
-            binding.content.clearFocus()
-            viewModel.cancelVM()
-            //binding.group1.setVisibility(View.GONE)
-        }
+//        viewModel.editedPostLD.observe(this) {
+//            binding.content.setText(it.content)
+//            binding.content.requestFocus()
+//            if (it.id != 0L) {
+//                binding.preview1.setText(it.author)
+//                binding.preview2.setText(it.published)
+//                binding.group1.setVisibility(View.VISIBLE)
+//            } else binding.group1.setVisibility(View.GONE)
+//        }
+//
+//        binding.saveButton.setOnClickListener {
+//            val text = binding.content.text.toString()
+//            if (text.isBlank()) {
+//                Toast.makeText(this, "error empty content", Toast.LENGTH_LONG).show()
+//                return@setOnClickListener
+//            }
+//            viewModel.saveVM(text)
+//            binding.content.setText("")
+//            binding.content.clearFocus()
+//        }
+//
+//        binding.cancelEdit.setOnClickListener {
+//            binding.content.setText("")
+//            binding.content.clearFocus()
+//            viewModel.cancelVM()
+//            //binding.group1.setVisibility(View.GONE)
+//        }
     }
 }
