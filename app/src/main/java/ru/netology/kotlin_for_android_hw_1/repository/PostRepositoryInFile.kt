e48927d92cc59278e936ce739bdfdfa7d6750d9d
Maksim Timofeyev <makstim@list.ru>
2024-12-18 22:41:48 +0300

    Commit HW10 Initial Commit

---
+++ app/src/main/java/ru/netology/kotlin_for_android_hw_1/repository/PostRepositoryInFile.kt
@@ -0,0 +1,140 @@
+package ru.netology.kotlin_for_android_hw_1.repository
+
+import android.content.Context
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import ru.netology.kotlin_for_android_hw_1.dto.Post
+import java.lang.IllegalStateException
+
+class PostRepositoryInFile(private val context: Context) : PostRepository {
+
+    companion object {
+        private val gson = Gson()
+        private val token = TypeToken.getParameterized(List::class.java, Post::class.java).type
+        private const val FILENAME = "posts.json"
+    }
+
+    private var nextPostID = 1L
+    private var posts = listOf<Post>(
+        Post(
+            id = nextPostID++,
+            author = "Нетология. Университет интернет-профессий будущего",
+            published = "21 мая в 18:36",
+            content = "Много лет размышлял я над жизнью земной.\n" +
+                    "Непонятного нет для меня под луной.\n" +
+                    "Мне известно, что мне ничего не известно!\n" +
+                    "Вот последняя правда, открытая мной. → https://library.vladimir.ru/spravochnyj-material/omar2.html"
+        ),
+        Post(
+            id = nextPostID++,
+            author = "Нетология. Университет интернет-профессий будущего",
+            published = "21 мая в 18:36",
+            content = "Я — школяр в этом лучшем из лучших миров.\n" +
+                    "Труд мой тяжек: учитель уж больно суров!\n" +
+                    "До седин я у жизни хожу в подмастерьях,\n" +
+                    "Все еще не зачислен в разряд мастеров… → https://library.vladimir.ru/spravochnyj-material/omar2.html"
+        ),
+        Post(
+            id = nextPostID++,
+            author = "Нетология. Университет интернет-профессий будущего",
+            published = "21 мая в 18:36",
+            content = "Лучше впасть в нищету, голодать или красть,\n" +
+                    "Чем в число блюдолизов презренных попасть.\n" +
+                    "Лучше кости глодать, чем прельститься сластями\n" +
+                    "За столом у мерзавцев, имеющих власть. → https://library.vladimir.ru/spravochnyj-material/omar2.html"
+        ),
+        Post(
+            id = nextPostID++,
+            author = "Нетология. Университет интернет-профессий будущего",
+            published = "21 мая в 18:36",
+            content = "Не оплакивай, смертный, вчерашних потерь,\n" +
+                    "Дел сегодняшних завтрашней меркой не мерь,\n" +
+                    "Ни былой, ни грядущей минуте не верь,\n" +
+                    "Верь минуте текущей — будь счастлив теперь! → https://library.vladimir.ru/spravochnyj-material/omar2.html"
+        ),
+        Post(
+            id = nextPostID++,
+            author = "Нетология. Университет интернет-профессий будущего",
+            published = "21 мая в 18:36",
+            content = "Если все государства, вблизи и вдали,\n" +
+                    "Покоренные, будут валяться в пыли —\n" +
+                    "Ты не станешь, великий владыка, бессмертным.\n" +
+                    "Твой удел невелик: три аршина земли. → https://library.vladimir.ru/spravochnyj-material/omar2.html"
+        ),
+        Post(
+            id = nextPostID++,
+            author = "Нетология. Университет интернет-профессий будущего",
+            published = "21 мая в 18:36",
+            content = "Привет, это новая Нетология! Когда-то Нетология начиналась с интенсивов по онлайн-маркетингу. Затем появились курсы по дизайну, разработке, аналитике и управлению. Мы растём сами и помогаем расти студентам: от новичков до уверенных профессионалов. Но самое важное остаётся с нами: мы верим, что в каждом уже есть сила, которая заставляет хотеть больше, целиться выше, бежать быстрее. Наша миссия — помочь встать на путь роста и начать цепочку перемен → http://netolo.gy/fyb",
+            video = "https://www.youtube.com/watch?v=WhWc3b3KhnY"
+        )
+    )
+        set(value) {
+            field = value
+            sync()
+        }
+
+    private val data = MutableLiveData(posts)
+
+    init {
+        val file = context.filesDir.resolve(FILENAME)
+        if (file.exists()) {
+            context.openFileInput(FILENAME).bufferedReader().use { it ->
+                posts = gson.fromJson(it, token)
+                nextPostID = posts.maxOf { it.id } + 1
+                data.value = posts
+            }
+        } else {
+            sync()
+        }
+    }
+
+    private fun sync() {
+        context.openFileOutput(FILENAME, Context.MODE_PRIVATE).bufferedWriter().use {
+            it.write(gson.toJson(posts))
+        }
+    }
+
+    override fun getPostsAll(): LiveData<List<Post>> {
+        return data
+    }
+
+    override fun likeByID(id: Long) {
+        posts = posts.map {
+            if (it.id != id) it else it.copy(
+                likesNum = if (it.likedByMe) it.likesNum - 1
+                else it.likesNum + 1,
+                likedByMe = !it.likedByMe
+            )
+        }
+        data.value = posts
+    }
+
+    override fun shareByID(id: Long) {
+        posts = posts.map {
+            if (it.id != id) it else it.copy(
+                sharesNum = it.sharesNum + 1
+            )
+        }
+        data.value = posts
+    }
+
+    override fun removeByID(id: Long) {
+        posts = posts.filter { it.id != id }
+        data.value = posts
+    }
+
+    override fun save(post: Post) {
+        posts = posts.plus(post.copy(id = nextPostID++, author = "Me", published = "Now"))
+        data.value = posts
+    }
+
+    override fun edit(post: Post) {
+        posts = posts.map {
+            if (it.id != post.id) it else it.copy(content = post.content)
+        }
+        data.value = posts
+    }
+}
--- app/src/main/java/ru/netology/kotlin_for_android_hw_1/viewmodel/PostViewModel.kt
+++ app/src/main/java/ru/netology/kotlin_for_android_hw_1/viewmodel/PostViewModel.kt
@@ -1,14 +1,18 @@
 package ru.netology.kotlin_for_android_hw_1.viewmodel
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import ru.netology.kotlin_for_android_hw_1.dto.Post
 import ru.netology.kotlin_for_android_hw_1.dto.postEmpty
+import ru.netology.kotlin_for_android_hw_1.repository.PostRepositoryInFile
 import ru.netology.kotlin_for_android_hw_1.repository.PostRepositoryInMemory
 
-class PostViewModel : ViewModel() {
+class PostViewModel (application: Application): AndroidViewModel(application) {
 
-    private val repository = PostRepositoryInMemory()
+    //    private val repository = PostRepositoryInMemory()
+    private val repository = PostRepositoryInFile(application)
 
     val data = repository.getPostsAll()
     val editedPostLD = MutableLiveData(postEmpty)
@@ -31,7 +35,7 @@ class PostViewModel : ViewModel() {
             repository.save(Post(content = content))
         } else {
             repository.edit(editedPost.copy(content = content))
-            editedPostLD.value= postEmpty
+            editedPostLD.value = postEmpty
         }
     }
 
