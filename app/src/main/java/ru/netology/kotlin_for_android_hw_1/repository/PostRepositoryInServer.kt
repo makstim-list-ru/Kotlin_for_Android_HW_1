75a85f6daa5b95b596703273ea5ec91d42ecc85f
Maksim Timofeyev <makstim@list.ru>
2025-02-16 00:03:31 +0300

    Commit HW3_prom_dev v_1

--- app/src/main/java/ru/netology/kotlin_for_android_hw_1/repository/PostRepositoryInServer.kt
+++ app/src/main/java/ru/netology/kotlin_for_android_hw_1/repository/PostRepositoryInServer.kt
@@ -5,13 +5,17 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
+import okhttp3.Call
+import okhttp3.Callback
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.RequestBody.Companion.toRequestBody
+import okhttp3.Response
 import okhttp3.internal.EMPTY_REQUEST
 import ru.netology.kotlin_for_android_hw_1.dto.Post
 import ru.netology.kotlin_for_android_hw_1.model.FeedModel
+import java.io.IOException
 import java.util.concurrent.TimeUnit
 import kotlin.concurrent.thread
 
@@ -112,6 +116,28 @@ class PostRepositoryInServer(context: Context) : PostRepository {
         }
         return data
     }
+
+//    fun getAllAsync(callback: PostRepository.GetAllCallback) {
+//        val request: Request = Request.Builder()
+//            .url("${BASE_URL}/api/slow/posts")
+//            .build()
+//
+//        client.newCall(request)
+//            .enqueue(object : Callback {
+//                override fun onResponse(call: Call, response: Response) {
+//                    val body = response.body?.string() ?: throw RuntimeException("body is null")
+//                    try {
+//                        callback.onSuccess(gson.fromJson(body, typeToken.type))
+//                    } catch (e: Exception) {
+//                        callback.onError(e)
+//                    }
+//                }
+//
+//                override fun onFailure(call: Call, e: IOException) {
+//                    callback.onError(e)
+//                }
+//            })
+//    }
 
     private fun serverStatusChange(status: String): FeedModel {
         return when (status) {
